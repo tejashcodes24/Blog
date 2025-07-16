9f3e50a5adbc8fdfbe279aa9bec4bd8d214fdba2
Tejash <tejashmaru2003@gmail.com>
2025-07-17 02:16:28 +0530

    feat: add Header component to App and clean up Home component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import Dashboard from './pages/Dashboard'
 import Project from './pages/Project'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import Header from './components/Header'
 import { BrowserRouter,Route,Routes } from 'react-router-dom';
 const App = () => {
   return (
     <BrowserRouter>
+    <Header/>    
       <Routes>
         <Route path='/' element= {<Home />}/>
         <Route path='/about' element = {<About />} />
