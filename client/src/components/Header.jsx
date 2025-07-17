8bcef1899e74e58ef2d22610dd9e649cc37c25b3
Tejash <tejashmaru2003@gmail.com>
2025-07-18 02:00:05 +0530

    fix: add outline prop to Sign In button in Header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -28,7 +28,7 @@ const Header = () => {
           <FaMoon />
         </Button>
         <Link to='/sign-in'>
-          <Button className="bg-gradient-to-br from-purple-600 to-blue-500 text-white hover:bg-gradient-to-bl focus:ring-blue-300 dark:focus:ring-blue-800">
+          <Button className="bg-gradient-to-br from-purple-600 to-blue-500 text-white hover:bg-gradient-to-bl focus:ring-blue-300 dark:focus:ring-blue-800" outline>
             Sign In 
           </Button>
         </Link>
