c4695ed3e5601de30cd9153a07ea37e92e9bee17
Tejash <tejashmaru2003@gmail.com>
2025-07-19 19:14:45 +0530

    fix: correct casing of the sign-up route path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
         <Route path='/' element= {<Home />}/>
         <Route path='/about' element = {<About />} />
         <Route path = '/sign-in' element = {<SignIn />} />
-        <Route path = '/Sign-up' element = {<SignUp />} />
+        <Route path = '/sign-up' element = {<SignUp />} />
         <Route path='/dashboard' element = {<Dashboard />} />
         <Route path='/project' element = {<Project />} />
       </Routes>
