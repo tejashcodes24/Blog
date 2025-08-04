5c6c5c2b28f0d584d9af4c8077c595107574d6d7
Tejash <tejashmaru2003@gmail.com>
2025-08-05 01:22:07 +0530

    add show more functionality  to the posts results of the dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom'
 const DashPosts = () => {
   const { currentUser } = useSelector((state) => state.user)
   const [userPosts, setUserPosts] = useState([])
+  const [showMore, setShowMore] = useState(true)
   useEffect(() => {
     const fetchPosts = async () => {
       try{
@@ -13,6 +14,9 @@ const DashPosts = () => {
         const data = await res.json()
         if(res.ok){
           setUserPosts(data.posts);
+          if(data.posts.length < 9){
+            setShowMore(false);
+          }
         }
       }
       catch(error){
@@ -22,7 +26,24 @@ const DashPosts = () => {
     if(currentUser.isAdmin){
       fetchPosts();
     }
-  }, [currentUser._id])
+  }, [currentUser._id]);
+
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+      const data = await res.json();
+      if(res.ok){
+        setUserPosts((prev) => [...prev,...data.posts]);
+        if(data.posts.length < 9){
+          setShowMore(false);
+        }
+      }
+    }
+    catch(error){
+
+    }
+  }
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar 
     scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700
@@ -75,6 +96,11 @@ const DashPosts = () => {
             ))}
           
         </Table>
+        {showMore && (
+          <button onClick={handleShowMore} className='w-full text-teal-500 self-center text-sm py-7'>
+            show more
+          </button>
+        )}
         </>
       ) : (
         <p>You have no post yet</p>
