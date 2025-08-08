acab29169eede07ecf3f07336f1abaed0781c123
Tejash <tejashmaru2003@gmail.com>
2025-08-08 18:03:27 +0530

    implement edit functionality for comments with UI and backend integration

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -79,6 +79,14 @@ const CommentSection = ({postId}) => {
         console.log(error);
       }
     }
+
+    const handleEdit = async (comment, editedContent) => {
+    setComments(
+      comments.map((c) =>
+        c._id === comment._id ? { ...c, content: editedContent } : c
+      )
+    );
+  };
   return (
     <div className='max-w-2xl mx-auto w-full p-3'>
       {currentUser ? 
@@ -136,6 +144,7 @@ const CommentSection = ({postId}) => {
               key={comment._id}
               comment={comment}
               onLike={handleLike}
+              onEdit= {handleEdit}
             />
           ))}
         </>
